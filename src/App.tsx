1686aab3a769218340a420a5bc610b801bdbc1da
wlals <jiminelp@gmail.com>
2024-08-04 23:21:06 +0900

    setting :: vite react tailwindcss typescript 기본 설정

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="font-pretendard text-purple text-title1">asdf</div>
+    </>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+// tailwind.config.js
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+    fontSize: {
+      title1: 64,
+      title2: 52,
+      title3: 48,
+      body1: 32,
+      body2: 20,
+      body3: 16,
+      tiny1: 12,
+    },
+    fontFamily: {
+      pretendard: ["Pretendard-Bold"],
+    },
+    colors: {
+      purple: "#7f57d2",
+      white: "#f4f5f9",
+    },
+  },
+  plugins: [],
+};
