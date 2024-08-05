d64e0e608e747a7318d64af42a00ad3478a98e1b
wlals <jiminelp@gmail.com>
2024-08-05 23:43:14 +0900

    feat :: 대충 section main 퍼블리싱

--- src/components/section.tsx
+++ src/components/section.tsx
@@ -4,10 +4,10 @@ const Section = () => {
   return (
     <div className="flex-col">
       <ImgSkeleton />
-      <div>
-        <p>제목</p>
-        <p>설명</p>
-        <p>날짜</p>
+      <div className="flex-col bg-[#fff] text-[#000] py-5 gap-2.5 px-5 font-pretendard">
+        <p className="flex w-full text-body2">제목</p>
+        <p className="flex w-full text-body3">설명</p>
+        <p className="flex w-full text-body3 justify-end">날짜</p>
       </div>
     </div>
   );
