b6e8def7c4e29d1cecb508b382233e3951174be0
wlals <jiminelp@dsm.hs.kr>
2024-11-17 18:08:42 +0900

    feat :: 디테일 api 연동

--- src/main/Detail.tsx
+++ src/main/Detail.tsx
@@ -1,14 +1,48 @@
-import { useNavigate, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import BigImgSkeleton from "../assets/bigImgSkeleton";
 import Button from "./Button";
 import Heart from "../assets/heart";
 import OutlineHeart from "../assets/outlineHeart";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import { getDetail, patchLikes, patchPublished } from "../api/details";
 
 const Detail = () => {
   const { id } = useParams();
-  const isMine = true;
+  const isMine = false;
   const [isHeart, setIsHeart] = useState(false);
+  const queryClient = useQueryClient();
+
+  const { data } = useQuery({
+    queryKey: ["detail", id],
+    queryFn: () => getDetail(id as string),
+  });
+
+  const { mutate: patchLike } = useMutation({
+    mutationKey: ["like", id],
+    mutationFn: () => patchLikes(id as string),
+    onSuccess: () =>
+      queryClient.invalidateQueries({
+        queryKey: ["detail"],
+      }),
+  });
+
+  const { mutate: patchPublish } = useMutation({
+    mutationKey: ["published", id],
+    mutationFn: () => patchPublished(id as string),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["detail"],
+      });
+    },
+  });
+
+  useEffect(() => {
+    if (data?.liked) {
+      setIsHeart(true);
+    }
+  }, [data]);
+
   return (
     <main className="bg-white flex flex-col gap-[30px] px-[50px] py-[10px] w-full h-full overflow-x-hidden font-pretendard items-center min-h-[100vh]">
       <div className="flex items-center justify-between min-w-full">
@@ -28,59 +62,57 @@ const Detail = () => {
         <div className="flex flex-col w-full">
           <div className="flex justify-between">
             <div className="flex items-end gap-4">
-              <span className="leading-none text-title3">
-                맛있는 제목이에요
-              </span>
-              {isMine && <span className="text-body2">공개</span>}
+              <span className="leading-none text-title3">{data?.title}</span>
+              {isMine && (
+                <span className="text-body2">
+                  {data?.published ? "공개" : "비공개"}
+                </span>
+              )}
             </div>
             {isMine ? (
               <div className="flex items-center gap-[10px]">
-                <Button label="비공개" className="text-white bg-purple" />
+                <Button
+                  label={data?.published ? "비공개" : "공개"}
+                  className="text-white bg-purple"
+                  onclick={() => patchPublish()}
+                />
                 <Button label="수정" className="text-black bg-white border" />
                 <Button label="삭제" className="text-white bg-red" />
               </div>
             ) : (
               <div className="flex items-end gap-[5px]">
                 <Heart />
-                <p className="leading-normal">234</p>
+                <p className="leading-normal">{data?.likes}</p>
               </div>
             )}
           </div>
           <div className="flex items-center justify-between">
-            <p className="text-body1">나무에서떨어진나무늘보</p>
-            <p>2024년 6월 3일</p>
+            <p className="text-body1">{data?.author}</p>
+            <p>
+              {`${data?.post_date.split("-")[0]}년 ${
+                data?.post_date.split("-")[1]
+              }월 ${data?.post_date.split("-")[2]}일`}
+            </p>
           </div>
         </div>
-        <BigImgSkeleton />
-        <div className="flex flex-col">
+        {data?.image_url ? <img src={data?.image_url} /> : <BigImgSkeleton />}
+        <div className="flex flex-col w-full">
           <p className="text-body1">내용</p>
-          <span className="text-body2">
-            이번엔 싱가포르를 다녀왔다.이번엔 싱가포르를 다녀왔다.이번엔
-            싱가포르를 다녀왔다.이번엔 싱가포르를 다녀왔다.이번엔 싱가포르를
-            다녀왔다.이번엔 싱가포르를 다녀왔다.이번엔 싱가포르를
-            다녀왔다.이번엔 싱가포르를 다녀왔다.이번엔 싱가포르를
-            다녀왔다.이번엔 싱가포르를 다녀왔다.이번엔 싱가포르를
-            다녀왔다.이번엔 싱가포르를 다녀왔다.이번엔 싱가포르를
-            다녀왔다.이번엔 싱가포르를 다녀왔다.이번엔 싱가포르를 다녀왔다.
-          </span>
+          <span className="text-body2">{data?.content}</span>
         </div>
-        <div className="flex flex-col">
+        <div className="flex flex-col w-full">
           <p className="text-body1">느낀점</p>
-          <span className="text-body2">
-            정말 재미가 있었다.정말 재미가 있었다.정말 재미가 있었다.정말 재미가
-            있었다.정말 재미가 있었다.정말 재미가 있었다.정말 재미가 있었다.정말
-            재미가 있었다.정말 재미가 있었다.정말 재미가 있었다.정말 재미가
-            있었다.정말 재미가 있었다.정말 재미가 있었다.정말 재미가 있었다.정말
-            재미가 있었다.정말 재미가 있었다.정말 재미가 있었다.정말 재미가
-            있었다.정말 재미가 있었다.정말 재미가 있었다.정말 재미가 있었다.
-          </span>
+          <span className="text-body2">{data?.feels}</span>
         </div>
         {!isMine && (
           <button
             className="flex p-[10px] gap-[5px] bg-purple items-center rounded-lg text-white w-fit"
-            onClick={() => setIsHeart(!isHeart)}
+            onClick={() => {
+              setIsHeart(!isHeart);
+              patchLike();
+            }}
           >
-            {isHeart ? <OutlineHeart /> : <Heart className="animate-scaleUp" />}
+            {isHeart ? <Heart className="animate-scaleUp" /> : <OutlineHeart />}
             <p>좋아요</p>
           </button>
         )}
